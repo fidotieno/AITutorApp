94addc1ae745b9aef531a9f1598cce7af57f92e9
owalkz <jjaraorie@gmail.com>
2025-05-24 03:17:48 +0300

    Modified CORS Options

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const corsOptions = {
   origin: [process.env.CLIENT_URL, "http://localhost:5173"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
